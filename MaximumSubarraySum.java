ddb09630c5f39f7e8e7e19edb581b35b083587bc
BrandonGangte <brandongangte@gmail.com>
2023-06-26 12:52:38 +0530

    Create MaximumSubarraySum.java
    Using Kadane's Algorithm

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,19 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+		long maxi = Integer.MIN_VALUE;
+        long sum = 0;
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            maxi = Math.max(maxi,sum);
+            if(sum<0) sum = 0;
+        }
+		if(maxi<=0) return 0;
+        return maxi;
+	}
+
+}
